2b95cbfa8b2035cef769314ac7391929db0cd626
Marco <Prosperi>
2020-06-12 16:46:28 +0200

    faccio partire l'alert, il countdown e le richieste all'utente

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@ Dopo che sono stati inseriti i 5 numeri,
 il software dice quanti e quali dei numeri da indovinare sono stati individuati
 */
 
-// Genero 5 numeri casuali e li metto in un alert
+// attendo il caricamento dell'HTML
 $(document).ready(
   function (){
+    var game = gameCountdown();
+    // Genero 5 numeri casuali
     function numeriCasuali(numeri) {
       var arrayNumeriCasuali = [];
       for (var i = 0; i < 5 ; i++) {
@@ -19,6 +21,31 @@ $(document).ready(
       console.log(arrayNumeriCasuali);
       return arrayNumeriCasuali;
     }
-    alert(numeriCasuali());
+    // e li metto in un alert.
+    // una volta che l'utente clicca ok faccio partire un timer di 30 secondi
+    function gameCountdown() {
+      var messaggio = alert('Ricordati questi numeri, al click su OK partira un countdown di 30 secondi e alla fine dovrai scrivere i numeri che avevi visto, sempre se ci riesci! ' + numeriCasuali());
+      setTimeout(
+        function () {
+          var arrayNumeriUtente = [];
+          for (var i = 0; i < 5; i++) {
+            var numeriUtente= parseInt(prompt('adesso dovrai scrivere, uno alla volta, i numeri che ricordi dei 5 che hai visto. Scrivi un numero e clicca OK'));
+            arrayNumeriUtente.push(numeriUtente);
+          }
+          console.log(arrayNumeriUtente);
+          return arrayNumeriUtente;
+        }
+      , 3000);
+    }
+    // alla fine dei 30 secondi partono i prompt dell'utente
+    // function () {
+    //   var arrayNumeriUtente = [];
+    //   for (var i = 0; i < 5; i++) {
+    //     var numeriUtente= parseInt(prompt('adesso dovrai scrivere, uno alla volta, i numeri che ricordi dei 5 che hai visto. Scrivi un numero e clicca OK'));
+    //     arrayNumeriUtente.push(numeriUtente);
+    //   }
+    //   console.log(arrayNumeriUtente);
+    //   return arrayNumeriUtente;
+    // }
   }
 );
